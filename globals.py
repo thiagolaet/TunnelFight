49e032d6a0ba59c096522ec90eebf75be3027c13
Thiago Laet <44308207+thiagolaet@users.noreply.github.com>
2019-06-07 06:54:29 -0300

    .

--- globals.py
+++ globals.py
@@ -1,7 +1,7 @@
 WIDTH = 800
 HEIGHT = 600
 
-# 1 - menu / 2 - jogo / 3 - dificuldade / 4 - ranking / 5 - sair
+# 1 - menu / 2 - jogo beat'em up/ 3 - jogo nave/ 5 - sair
 GAME_STATE = 1
 
 # 0 - fácil / 1 - médio / 2 - difícil
