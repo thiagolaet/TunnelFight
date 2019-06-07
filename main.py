3b9ab49a660b092abcf6c9e135bb7086f9fd9504
Thiago Laet <44308207+thiagolaet@users.noreply.github.com>
2019-06-07 06:48:45 -0300

    Modularização e Contador de ataque do player

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from PPlay.sprite import *
 from PPlay.gameimage import *
 from PPlay.gameobject import *
 from PPlay.animation import *
+from player import Player
+import globals
 
-janela = Window(1000, 800)
+
+janela = Window(globals.WIDTH, globals.HEIGHT)
 teclado = janela.get_keyboard()
 janela.set_title("Daft Drunk")    
 
@@ -13,103 +16,21 @@ background = Sprite("assets/background.png", 1)
 frame_per_SECOND = 60
 speed_per_FRAME = 60
 
-#player
-idle_direita = Animation("assets/player-idle-right-2.png", 4)
-idle_esquerda = Animation("assets/player-idle-left-2.png", 4)
-andando_direita = Animation("assets/player-walking-right-2.png", 8)
-andando_esquerda = Animation("assets/player-walking-left-2.png", 8)
-atacando_esquerda = Animation("assets/player-attack-left.png", 4)
-atacando_direita = Animation("assets/player-attack-right2.png", 9)
-
-idle_direita.set_total_duration(800)
-idle_esquerda.set_total_duration(3000)
-andando_direita.set_total_duration(800)
-andando_esquerda.set_total_duration(800)
-atacando_direita.set_total_duration(800)
-atacando_esquerda.set_total_duration(800)
-
-player = idle_direita
-
-player.set_position(100, janela.height - 100)
-atacando_direita.set_position(100, janela.height - 100)
-atacando_direita.set_position(100, janela.height - 100)
-andando_esquerda.set_position(100, janela.height - 100)
-andando_direita.set_position(100, janela.height - 100)
-idle_esquerda.set_position(100, janela.height - 100)
-
-
 direcao = 1
 #1 = direita / 2 = esquerda
 
+player = Player(janela)
+
+def checarLimitesJogador():
+    if player.player.y < 260:
+        player.set_pos_y(260)
+    if player.player.y > 470:
+        player.set_pos_y(470)
+
 while True:
-    janela.set_background_color((255,255,255))
     background.draw()
-    if(teclado.key_pressed("D")):
-        player = andando_direita
-        player.x += 0.4
-        idle_direita.x += 0.4
-        idle_esquerda.x += 0.4
-        andando_esquerda.x += 0.4
-        atacando_esquerda.x += 0.4
-        atacando_direita.x += 0.4
-        direcao = 1
-    elif(teclado.key_pressed("A")):
-        player = andando_esquerda
-        player.x -= 0.4
-        idle_direita.x -= 0.4
-        idle_esquerda.x -= 0.4
-        andando_direita.x -= 0.4
-        atacando_esquerda.x-= 0.4
-        atacando_direita.x -= 0.4
-        direcao = 0
-    else: 
-        if(direcao == 1):
-            player = idle_direita
-        elif(direcao == 0):
-            player = idle_esquerda
+    
+    checarLimitesJogador()
 
-    if(teclado.key_pressed("S")):
-        if(direcao == 1):
-            player = andando_direita
-            player.y += 0.4
-            idle_direita.y += 0.4
-            idle_esquerda.y += 0.4
-            andando_esquerda.y += 0.4
-            atacando_esquerda.y += 0.4
-            atacando_direita.y += 0.4
-        else:
-            player = andando_esquerda
-            player.y += 0.4
-            idle_direita.y += 0.4
-            idle_esquerda.y += 0.4
-            andando_direita.y += 0.4
-            atacando_esquerda.y += 0.4
-            atacando_direita.y += 0.4
-
-    if(teclado.key_pressed("W")):
-        if(direcao == 1):
-            player = andando_direita
-            player.y -= 0.4
-            idle_direita.y -= 0.4
-            idle_esquerda.y -= 0.4
-            andando_esquerda.y -= 0.4
-            atacando_esquerda.y -= 0.4
-            atacando_direita.y -= 0.4
-        else:
-            player = andando_esquerda
-            player.y -= 0.4
-            idle_direita.y -= 0.4
-            idle_esquerda.y -= 0.4
-            andando_direita.y -= 0.4
-            atacando_direita.y -= 0.4
-            atacando_esquerda.y -= 0.4
-
-    if(teclado.key_pressed("I")):
-        if direcao == 1:
-            player = atacando_direita
-        else:
-            player = atacando_esquerda
-
-    player.draw()
-    player.update()
+    player.run()
     janela.update()
--- player.py
+++ player.py
@@ -1,49 +1,71 @@
 from PPlay.window import *
 from PPlay.sprite import *
 from PPlay.gameimage import *
-from PPlay.gameobect import *
+from PPlay.gameobject import *
 from PPlay.animation import *
 
 class Player():
-    def __init__(self, direcao):
-        self.idle_direita = Animation("assets/player-idle-right.png", 4)
-        self.idle_esquerda = Animation("assets/player-idle-left.png", 4)
-        self.andando_direita = Animation("assets/player-walking-right.png", 8)
-        self.andando_esquerda = Animation("assets/player-walking-left.png", 8)
-        self.atacando_esquerda = Animation("assets/player-attack-left.png", 4)
-        self.atacando_direita = Animation("assets/player-attack-right.png", 4)
-
+    def __init__(self, janela):
+        self.idle_direita = Animation("assets/player-idle-right-2.png", 4)
+        self.idle_esquerda = Animation("assets/player-idle-left-2.png", 4)
+        self.andando_direita = Animation("assets/player-walking-right-2.png", 8)
+        self.andando_esquerda = Animation("assets/player-walking-left-2.png", 8)
+        self.atacando_esquerdaI = Animation("assets/player-attack-left-2.png", 9)
+        self.atacando_direitaI = Animation("assets/player-attack-right-2.png", 9)
+        self.player = self.idle_direita = Animation("assets/player-idle-right-2.png", 4)
         self.idle_direita.set_total_duration(800)
         self.idle_esquerda.set_total_duration(800)
         self.andando_direita.set_total_duration(800)
         self.andando_esquerda.set_total_duration(800)
-        self.atacando_direita.set_total_duration(800)
-        self.atacando_esquerda.set_total_duration(800)
+        self.atacando_direitaI.set_total_duration(800)
+        self.atacando_esquerdaI.set_total_duration(800)
+        self.janela = janela
+        self.teclado = janela.get_keyboard()
+        self.direcao = 1
+        self.set_pos(100, self.janela.height - 200)
 
-        self.direcao = direcao
+        self.contador = 0
+        #1 - pode fazer tudo / 2 - tem que esperar a animação atual acabar
+        self.estado = 1
+        self.vida = 50
 
 
-    def andarDireita(self, player, direcao):
+    def set_pos(self, x, y):
+        self.player.set_position(x, y)
+        self.atacando_direitaI.set_position(x, y)
+        self.atacando_esquerdaI.set_position(x, y)
+        self.andando_esquerda.set_position(x, y)
+        self.andando_direita.set_position(x, y)
+        self.idle_esquerda.set_position(x, y)
+
+    def set_pos_y(self, y):
+        self.player.set_position(self.player.x, y)
+        self.atacando_direitaI.set_position(self.player.x, y)
+        self.atacando_esquerdaI.set_position(self.player.x, y)
+        self.andando_esquerda.set_position(self.player.x, y)
+        self.andando_direita.set_position(self.player.x, y)
+        self.idle_esquerda.set_position(self.player.x, y)
+        self.idle_direita.set_position(self.player.x, y)
+
+    def andarDireita(self):
         self.player = self.andando_direita
-        self.player.x += 0.1
-        self.idle_direita.x += 0.1
-        self.idle_esquerda.x += 0.1
-        self.andando_esquerda.x += 0.1
-        self.atacando_esquerda.x += 0.1
-        self.atacando_direita.x += 0.1
-        direcao = 1
-        return direcao
+        self.player.x += 0.4
+        self.idle_direita.x += 0.4
+        self.idle_esquerda.x += 0.4
+        self.andando_esquerda.x += 0.4
+        self.atacando_esquerdaI.x += 0.4
+        self.atacando_direitaI.x += 0.4
+        self.direcao = 1
 
     def andarEsquerda(self):
         self.player = self.andando_esquerda
-        self.player.x -= 0.1
-        self.idle_direita.x -= 0.1
-        self.idle_esquerda.x -= 0.1
-        self.andando_direita.x -= 0.1
-        self.atacando_esquerda.x-= 0.1
-        self.atacando_direita.x -= 0.1
-        direcao = 0
-        return direcao
+        self.player.x -= 0.4
+        self.idle_direita.x -= 0.4
+        self.idle_esquerda.x -= 0.4
+        self.andando_direita.x -= 0.4
+        self.atacando_esquerdaI.x-= 0.4
+        self.atacando_direitaI.x -= 0.4
+        self.direcao = 0
 
     def idle(self):
         if(self.direcao == 1):
@@ -53,32 +75,82 @@ class Player():
 
     def andarDireitaBaixo(self):
             self.player = self.andando_direita
-            self.player.y += 0.08
-            self.idle_direita.y += 0.08
-            self.idle_esquerda.y += 0.08
-            self.andando_esquerda.y += 0.08
-            self.atacando_esquerda.y += 0.08
-            self.atacando_direita.y += 0.08
+            self.player.y += 0.3
+            self.idle_direita.y += 0.3
+            self.idle_esquerda.y += 0.3
+            self.andando_esquerda.y += 0.3
+            self.atacando_esquerdaI.y += 0.3
+            self.atacando_direitaI.y += 0.3
     def andarEsquerdaBaixo(self):
             self.player = self.andando_esquerda
-            self.player.y += 0.08
-            self.idle_direita.y += 0.08
-            self.idle_esquerda.y += 0.08
-            self.andando_direita.y += 0.08
-            self.atacando_esquerda.y += 0.08
-            self.atacando_direita.y += 0.08
+            self.player.y += 0.3
+            self.idle_direita.y += 0.3
+            self.idle_esquerda.y += 0.3
+            self.andando_direita.y += 0.3
+            self.atacando_esquerdaI.y += 0.3
+            self.atacando_direitaI.y += 0.3
     def andarDireitaCima(self):
             self.player = self.andando_direita
-            self.player.y -= 0.08
-            self.idle_direita.y -= 0.08
-            self.idle_esquerda.y -= 0.08
-            self.andando_esquerda.y -= 0.08
-            self.atacando_esquerda.y -= 0.08
-            self.atacando_direita.y -= 0.08
+            self.player.y -= 0.3
+            self.idle_direita.y -= 0.3
+            self.idle_esquerda.y -= 0.3
+            self.andando_esquerda.y -= 0.3
+            self.atacando_esquerdaI.y -= 0.3
+            self.atacando_direitaI.y -= 0.3
     def andarEsquerdaCima(self):
             self.player = self.andando_esquerda
-            self.player.y -= 0.08
-            self.idle_direita.y -= 0.08
-            self.idle_esquerda.y -= 0.08
-            self.andando_direita.y -= 0.08
-            self.atacando_esquerda.y -= 0.08
+            self.player.y -= 0.3
+            self.idle_direita.y -= 0.3
+            self.idle_esquerda.y -= 0.3
+            self.andando_direita.y -= 0.3
+            self.atacando_esquerdaI.y -= 0.3
+            self.atacando_direitaI.y -= 0.3
+
+    def atacarDireitaI(self):
+        self.player = self.atacando_direitaI
+        self.estado = 2
+        self.contador = 0
+
+    def atacarEsquerdaI(self):
+        self.player = self.atacando_esquerdaI
+        self.estado = 2
+        self.contador = 0
+    
+
+    def run(self):
+        self.player.draw()
+        self.player.update()
+        if self.estado != 2:
+            if(self.teclado.key_pressed("D")):
+                self.andarDireita()
+            elif(self.teclado.key_pressed("A")):
+                self.andarEsquerda()
+            else: 
+                self.idle()
+
+            if(self.teclado.key_pressed("S")):
+                if(self.direcao == 1):
+                    self.andarDireitaBaixo()
+                else:
+                    self.andarEsquerdaBaixo()
+
+            if(self.teclado.key_pressed("W")):
+                if(self.direcao == 1):
+                    self.andarDireitaCima()
+                else:
+                    self.andarEsquerdaCima()
+            
+            if(self.contador > 0.20):
+                if(self.teclado.key_pressed("I")):
+                    if(self.direcao == 1):
+                        self.atacarDireitaI()
+                    else:
+                        self.atacarEsquerdaI()
+        else:
+            if self.contador > 0.7:
+                self.estado = 1
+                self.atacando_direitaI.set_curr_frame(0)
+                self.atacando_esquerdaI.set_curr_frame(0)
+                self.contador = 0
+
+        self.contador += self.janela.delta_time()
