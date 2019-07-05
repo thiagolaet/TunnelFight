827c277dfec784f0af094663b60715df165a41b7
João Vicente <gaidzinski2110@gmail.com>
2019-07-05 09:16:53 -0300

    ajuste gameover

--- game_over.py
+++ game_over.py
@@ -10,6 +10,7 @@ class GameOver(object):
     def __init__(self, janela):
         self.janela = janela
         self.distancia = 60
+        self.pontuacao = 0
         self.background = Sprite("assets/bg-fase1-menu.png")
         self.bg = Sprite("assets/background-gameover.png")
 
@@ -26,9 +27,12 @@ class GameOver(object):
         self.bg.draw()
 
 
-    def run(self):
+    def run(self, pontuacao):
         self._draw()
         self.set_pos()
+        self.pontuacao = pontuacao
+        self.janela.draw_text("Pontuação: "+str(self.pontuacao), self.janela.width/2, self.janela.height/2, size=30, color=(255, 255, 255), font_name="Minecraft")
+
         if self.janela.get_keyboard().key_pressed("ESC"):
             globals.GAME_STATE = 1
         
--- main.py
+++ main.py
@@ -32,7 +32,7 @@ while True:
     elif(globals.GAME_STATE == 3):
         ranking.run()
     elif globals.GAME_STATE == 4:
-        gameover.run()
+        gameover.run(jogo.gameController.pontuacao)
 
     else:
         globals.GAME_STATE = 1
